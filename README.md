97e2cf5e12d328a3dd8fd71c11ad7081e178e82c
Ziyuan Guo <s10e.cn@gmail.com>
2022-01-16 13:31:40 +0900

    add basic function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rpi-pico-midi-pedal
+
+Use Raspberry Pi Pico as a usb-midi interface for up to 3 continuous pedals and a switch pedal.
---
+++ code.py
@@ -0,0 +1,42 @@
+import board
+import analogio
+import digitalio
+import pwmio
+import time
+import usb_midi
+import adafruit_midi
+from adafruit_midi.control_change import ControlChange
+
+midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
+
+led = pwmio.PWMOut(board.LED)
+led_assigned_pedal = 0
+
+pedal_count = 4
+# sustain, sostenuto, harmonic and una corda
+pedal_cc = [64, 66, 69, 67]
+pedal_pin = [analogio.AnalogIn(board.A0),
+             analogio.AnalogIn(board.A1),
+             analogio.AnalogIn(board.A2),
+             digitalio.DigitalInOut(board.GP22)]
+pedal_value = [0, 0, 0, 0]
+pedal_deadzone = [0, 0, 0, 0]
+
+for i in range(pedal_count):
+    if type(pedal_pin[i]) == digitalio.DigitalInOut:
+        pedal_pin[i].direction = digitalio.Direction.INPUT
+
+while True:
+    for i in range(pedal_count):
+        print(type(pedal_pin[i]))
+        print(pedal_pin[i].value)
+        val = (pedal_pin[i].value // 512
+               if type(pedal_pin[i]) == analogio.AnalogIn
+               else pedal_pin[i].value * 127)
+
+        # send midi message only if the value is changed
+        if val != pedal_value[i]:
+            if i == led_assigned_pedal:
+                led.duty_cycle = val * 512
+            midi.send(ControlChange(pedal_cc[i], val))
+            pedal_value[i] = val
